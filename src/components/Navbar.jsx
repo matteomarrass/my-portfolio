fa2fcbc81aa0b5bdcbba5cdbabcc5bc27cebc916
matteomarrass <matteommarras@gmail.com>
2023-02-08 13:01:53 +0100

    Improved stle for bar and homepage IT

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,11 @@
 import React from "react";
-import { NavLink } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import './app.css'
 
 function Navbar(){
     return(
         <nav>
+            <Link to='/'><h2 id="titleCurriculum">Curriculum Vitae di Matteo Marras</h2></Link>
             <ul>
                 <NavLink to='/' className='link'>Cambia Lingua</NavLink>
                 <NavLink to='/homeit' className='link'>Panoramica</NavLink>
--- src/components/StartingPage.jsx
+++ src/components/StartingPage.jsx
@@ -9,7 +9,7 @@ function StartingPage() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <div className='firstDescription'>
-          <h1>Hey, io sono Matteo Marras, Jr. Front End Web Developer; benvenuto nel mio curriculum!</h1>
+          <h1 id='title'>Hey, io sono Matteo Marras, Jr. Front End Web Developer; benvenuto nel mio curriculum!</h1>
           <p>Sono un aspirante Front End Web developer alla ricerca della mia prima esperienza da dipendente sul campo.</p>
           <p>Il mio obiettivo è creare delle esperienze utente che siano fresche, d'impatto e che trasmettano positività al mondo.</p>
           <p>Quì dentro ho cercato di creare una panoramica di base della persona che sono, in aggiunta a un paio di progetti! Spero che sia interessato!</p>
