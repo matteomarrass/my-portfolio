f61c33dd2d32718dba0a2e1f8e31d8dba281e837
matteomarrass <matteommarras@gmail.com>
2023-02-09 16:53:23 +0100

    Production Build Created

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -6,7 +6,7 @@ const Projects = () => {
       <>
       <Navbar />
       <section id="studyPage">
-          <h1 className="titleStudy">Bene. Siamo quasi arrivati alla fine! <br />Ecco quà qualche piccolo progetto per completare il tutto! 🚧</h1>
+          <h1 className="titleStudy">Bene. Siamo quasi arrivati alla fine! <br />Ecco qualche piccolo progetto! 🚧</h1>
           <CardStudy />
       </section>
       </>
